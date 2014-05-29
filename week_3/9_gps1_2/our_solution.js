30c5ef678c08a033d18800647e516e3fecf7b8cc
scyclow <spike716@gmail.com>
2014-05-29 00:02:51 -0500

    completed gps 1.2

--- week_3/9_gps1_2/our_solution.js
+++ week_3/9_gps1_2/our_solution.js
@@ -1,45 +1,34 @@
 // GPS 1.2 - JavaScript
 
 // YOUR FULL NAMES:
-//  1.
-//  2.
+//  1. Steve Pikelny
+//  2. Clayton Jordan
 
 
-// 0. "YOU SIGNED... WHO?!"
+function Client(name, age, quote) {
+    this.name = name;
+    this.age = age;
+    this.quote = quote;
+    this.showQuote = function() {
+        console.log("My client, " + this.name + " is " + this.age + '-years-old. Most known for the quote: "' + this.quote + '"')
+    }
+}
 
+var adamSandler = new Client("Adam Sandler", 47, "That's your home! Are you too good for your home?!")
 
+var kristenBell = new Client("Kristen Bell", 33, "Do you wanna build a snowman?")
 
+var jimCarrey = new Client ("Jim Carrey", 52, "...So you're telling me there's a chance? YEAH!")
 
-// 1. "Here they Come!"
+console.log("My client, " + adamSandler.name + " is " + adamSandler.age + "-years-old. He's known for the quote: " +'"'+adamSandler.quote+'"')
+console.log("My client, " + kristenBell.name + " is " + kristenBell.age + "-years-old. She's known for the quote: " +'"'+kristenBell.quote+'"')
+console.log("My client, " + jimCarrey.name + " is " + jimCarrey.age + "-years-old. He's known for the quote: " +'"'+jimCarrey.quote+'"')
 
-
-
-
-
-// 2. "TIME IS MONEY!"
-
-//YOUR CODE HERE!
-
-var shooterMcGavin = new Client("Shooter McGavin", 48, "Just stay out of my way... or you'll pay. Listen to what I say.");
-shooterMcGavin.constructor === Client;
-shooterMcGavin.age === 48;
-shooterMcGavin.quote === "Just stay out of my way... or you'll pay. Listen to what I say.";
-
-
-
-
-
-// 3. "SHOW 'EM OFF!"
-
-
-
-
-
-// 4. "But wait, there's more!"
-
-
-
-// 5.  ** BONUS **
+adamSandler.showQuote()
 
 
 //  6.  Your Reflection:
+// Some of this stuff was relatively straightforwards. I think we had the most trouble trying to figure out what, exactly, the directions were asking of us.
+// Aside from that, we had to google some of the syntax for writing object-oriented JavaScript. 
+//I don't think that defining variables and objects is very intuative. At least, it's not as intuatuve as it is in Ruby or Python.
+//
