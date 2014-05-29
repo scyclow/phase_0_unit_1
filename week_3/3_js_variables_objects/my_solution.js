74ab95e7219ca412f8b2f31103677701cb9726e8
Steven Pikelny <spike716@gmail.com>
2014-05-28 20:14:58 -0500

    found solutions for all 9 exercises

--- week_3/3_js_variables_objects/my_solution.js
+++ week_3/3_js_variables_objects/my_solution.js
@@ -1,15 +1,28 @@
-// I paired [by myself, with:] on this challenge.
+
+// I paired [with Sebastian Radloff] on this challenge.
+//Driver1: Sebastian
+//Navigator1: Steve
+
 
 // __________________________________________
 // Write your code below.
 
+var secretNumber = 12;
 
+secretNumber = 7;
 
+var password = "string";
 
+password = "just open the door";
 
+var allowedIn = true;
 
+allowedIn = false;
 
+var members = [];
 
+members[0] = "John";
+members[3] = "Mary";
 // __________________________________________
 // Reflection: Use the reflection guidelines to write a reflection here. 
 // 
