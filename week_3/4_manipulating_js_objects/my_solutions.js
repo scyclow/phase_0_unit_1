854de6d7db0c942e8ab0ed89bc8b6322ea1ebd47
scyclow <spike716@gmail.com>
2014-05-30 12:51:51 -0500

    completed 3.4

---
+++ week_3/4_manipulating_js_objects/my_solutions.js
@@ -0,0 +1,125 @@
+
+// There is a section below where you will write your code.
+// Do not alter this object here.
+
+
+var terah = {
+  name: "Terah",
+  age: 32,
+  height: 66,
+  weight: 130,
+  hairColor: "brown",
+  eyeColor: "brown"
+}
+
+
+
+// __________________________________________
+// Write your code below.
+
+var adam = {};
+
+adam.name = "Adam";
+terah.spouse = adam;
+terah.weight = 125;
+delete terah.eyeColor;
+adam.spouse = terah;
+terah.children = {};
+terah.children.carson = {name: "Carson"};
+terah.children.carter = {name: "Carter"};
+terah.children.colton = {name: "Colton"};
+adam.children = terah.children
+
+
+
+
+
+// __________________________________________
+// Reflection: Use the reflection guidelines
+// 
+// 
+// This exercise was also pretty easy. But, it definitely helped solidify my understanding of creating objects with different properties in JS. 
+// I still think that it's kind of weird how JS creates objects. Where ruby and python require you to create a class and define attributes and parameters, JS objects are essentially hashes; and to generalize this creation, you basically make a function that makes the object.
+// 
+// 
+
+
+// __________________________________________
+// Driver Code:  Do not alter code below this line.
+function assert(test, message, test_number) {
+  if (!test) {
+    console.log(test_number + "false");
+    throw "ERROR: " + message;
+  }
+  console.log(test_number + "true");
+  return true;
+}
+
+assert(
+  (adam instanceof Object),
+  "The value of adam should be an Object.",
+  "1. "
+)
+
+assert(
+  (adam.name === "Adam"),
+  "The value of the adam name property should be 'Adam'.",
+  "2. "
+)
+
+assert(
+  terah.spouse === adam,
+  "terah should have a spouse property with the value of the variable adam.",
+  "3. "
+)
+
+assert(
+  terah.weight === 125,
+  "The terah weight property should be 125.",
+  "4. "
+)
+
+assert(
+  terah.eyeColor === undefined,
+  "The terah eyeColor property should be removed.",
+  "5. "
+)
+
+assert(
+  terah.spouse.spouse === terah,
+  "The terah spouse property's value spouse property should be terah.",
+  "6. "
+)
+
+assert(
+  (terah.children instanceof Object),
+  "The value of the terah children property should be an Object.",
+  "7. "
+)
+
+assert(
+  terah.children.carson.name === "Carson",
+  "The terah children property should have a carson property with its own property name with a value of 'Carson'.",
+  "8. "
+)
+
+assert(
+  terah.children.carter.name === "Carter",
+  "The terah children property should have a carter property with its own property name with a value of 'Carter'.",
+  "9. "
+)
+
+assert(
+  terah.children.colton.name === "Colton",
+  "The terah children property should have a colton property with its own property name with a value of 'Colton'.",
+  "10. "
+)
+
+assert(
+  adam.children === terah.children,
+  "The value of the adam children property should be the value of the terah children property",
+  "11. "
+)
+
+console.log("\nHere is your final terah object:")
+console.log(terah)
