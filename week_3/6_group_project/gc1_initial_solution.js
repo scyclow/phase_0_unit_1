23e21ade210cfaa22b491b463af1981563678d0c
scyclow <spike716@gmail.com>
2014-05-31 09:58:46 -0500

    finished group proj

--- week_3/6_group_project/gc1_initial_solution.js
+++ week_3/6_group_project/gc1_initial_solution.js
@@ -2,17 +2,19 @@
 
 // Here is my part of the challenge:
 
+
 var total = 0;
 for (var i=0; i < number.length; i++){
   total = total + number[i]; 
 }
 
-var mean = total/numbers.length
+var mean = total/number.length;
 
-number = number.sort()
+number = number.sort();
 
 if ((number.length/2)%2 === 0) {
-	return number[(number.length/2)-1]
+	number[(number.length/2)-1]
 } else {
-	return number[number.length%2]
+	number[number.length%2]
 }
+
